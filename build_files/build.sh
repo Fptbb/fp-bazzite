9ee2cf1f5f44c7951d79fdc3388ae1f56713edbf
Lucas Oliveira <lucas@fptbb.com>
2025-05-09 08:16:15 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,6 +3,29 @@
 RELEASE="$(rpm -E %fedora)"
 set -ouex pipefail
 
+### Install Fonts
+
+FONT_DIR="/usr/share/fonts/nerdfonts"
+mkdir -p "$FONT_DIR"
+
+install_nerd_font() {
+    local font_name="$1"
+    local font_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/${font_name}.zip"
+
+    echo "Installing Nerd Font: $font_name"
+    cd "$FONT_DIR"
+    curl -fLo "${font_name}.zip" "$font_url"
+    unzip -o "${font_name}.zip" -d "${font_name}"
+    rm -f "${font_name}.zip"
+}
+
+# Install desired fonts
+install_nerd_font "FiraCode"
+install_nerd_font "FiraMono"
+
+# Update font cache
+fc-cache -fv
+
 enable_copr() {
     repo="$1"
     repo_with_dash="${repo/\//-}"
@@ -25,7 +48,8 @@ dnf5 install -y \
     kleopatra \
     gnome-disk-utility \
     kitty \
-    zsh
+    zsh \
+    gnome-calculator
 
 # Use a COPR Example:
 #
